33dbbe2107624fefd2580ffca7258e8f9907e5d5
Xuedong Din <Xuedong.Din@Clarivate.com>
2024-01-18 16:01:19 -0500

    Create java project

---
+++ src/main/java/com/xdd/App.java
@@ -0,0 +1,13 @@
+package com.xdd;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
